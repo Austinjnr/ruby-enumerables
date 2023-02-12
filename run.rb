6f9a7bfe2219e58b24f52a0db2be13a6baf0108d
austinjnr <austinobonyo@gmail.com>
2023-02-12 23:09:25 +0300

    creates the array

---
+++ run.rb
@@ -0,0 +1,38 @@
+#array
+
+books = [
+
+    {
+        title: "Book 1",
+        price: 25.65,
+        author: "Author A",
+        publisher: "Publisher A"
+        rating: "Rating A",
+        description: "Description A"
+    },
+    {
+        title: "Book 2",
+        price: 26.05,
+        author: "Author B",
+        publisher: "Publisher B"
+        rating: "Rating B",
+        description: "Description B"
+    },
+    {
+        title: "Book 3",
+        price: 20.65,
+        author: "Author C",
+        publisher: "Publisher C"
+        rating: "Rating C",
+        description: "Description C"
+    },
+    {
+        title: "Book 4",
+        price: 25.55,
+        author: "Author D",
+        publisher: "Publisher D"
+        rating: "Rating D",
+        description: "Description D"
+    }
+
+]

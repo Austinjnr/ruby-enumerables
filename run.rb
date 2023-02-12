1cf6d377ca833490d203315887a6d02e295c03f9
austinjnr <austinobonyo@gmail.com>
2023-02-12 23:20:41 +0300

    practice on each method

--- run.rb
+++ run.rb
@@ -1,12 +1,11 @@
+require "pry"
 #array
-
 books = [
-
     {
         title: "Book 1",
         price: 25.65,
         author: "Author A",
-        publisher: "Publisher A"
+        publisher: "Publisher A",
         rating: "Rating A",
         description: "Description A"
     },
@@ -14,7 +13,7 @@ books = [
         title: "Book 2",
         price: 26.05,
         author: "Author B",
-        publisher: "Publisher B"
+        publisher: "Publisher B",
         rating: "Rating B",
         description: "Description B"
     },
@@ -22,7 +21,7 @@ books = [
         title: "Book 3",
         price: 20.65,
         author: "Author C",
-        publisher: "Publisher C"
+        publisher: "Publisher C",
         rating: "Rating C",
         description: "Description C"
     },
@@ -30,9 +29,17 @@ books = [
         title: "Book 4",
         price: 25.55,
         author: "Author D",
-        publisher: "Publisher D"
+        publisher: "Publisher D",
         rating: "Rating D",
         description: "Description D"
     }
-
 ]
+# Enumerable methods (that allow one to traverse, search and sort)
+ 
+#each method
+#loops through array
+def print_books
+    books.each do |b|
+        puts b
+    end
+end
